9ddccb8c75492f66b9a9a00abb111446f1ef7fc3
Xiaobonor <xiao.bo.nor@gmail.com>
2024-07-12 21:29:35 +0800

    tool function, and add license, readme

--- openai_assistant/__init__.py
+++ openai_assistant/__init__.py
@@ -3,3 +3,4 @@ from .utils import init
 from .assistant_manager import *
 from .thread_manager import *
 from .openai_assistant import OpenAIAssistant
+from .tool_functions_map import register_function, register_functions
--- openai_assistant/openai_assistant.py
+++ openai_assistant/openai_assistant.py
@@ -2,6 +2,7 @@
 import time
 from .thread_manager import create_thread, list_messages, send_message, delete_thread, \
     create_run, get_runs_by_thread, submit_tool_outputs_and_poll
+from .tool_functions_map import get_function
 
 
 class OpenAIAssistant:
@@ -37,6 +38,8 @@ class OpenAIAssistant:
             runs = await get_runs_by_thread(self.thread_id)
             running = runs.data[0]
             if running.status == "requires_action":
+                tool_outputs = await self._handle_tool_calls(running.required_action.submit_tool_outputs.tool_calls)
+                await submit_tool_outputs_and_poll(self.thread_id, running.id, tool_outputs)
                 await self._poll_until_complete(running.id)
                 break
             elif running.status in ["completed", "failed"]:
@@ -48,11 +51,33 @@ class OpenAIAssistant:
             runs = await get_runs_by_thread(self.thread_id)
             running = runs.data[0]
             if running.status in ["requires_action"]:
+                tool_outputs = await self._handle_tool_calls(running.required_action.submit_tool_outputs.tool_calls)
+                await submit_tool_outputs_and_poll(self.thread_id, running.id, tool_outputs)
                 await self._poll_until_complete(running.id)
                 break
             elif running.status in ["completed", "failed"]:
                 break
             time.sleep(2)
+
+    async def _handle_tool_calls(self, tool_calls):
+        output_list = []
+        for tool_call in tool_calls:
+            function_name = tool_call.function.name
+            function_args = tool_call.function.arguments
+
+            if isinstance(function_args, str):
+                function_args = eval(function_args.replace("true", "True").replace("false", "False"))
+
+            function = get_function(function_name)
+            if function:
+                output = await function(**function_args)
+                if output is None:
+                    output = "Got Error while processing this tool call."
+                output_list.append({
+                    "tool_call_id": tool_call.id,
+                    "output": output
+                })
+        return output_list
 
     async def _retrieve_latest_assistant_response(self):
         messages = await list_messages(self.thread_id)
