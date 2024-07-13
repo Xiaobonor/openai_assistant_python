6412ebb80892f12fc9a6606c2c95b181dcb69ba2
Xiaobonor <xiao.bo.nor@gmail.com>
2024-07-13 21:53:48 +0800

    optimization and add function tool

--- openai_assistant/__init__.py
+++ openai_assistant/__init__.py
@@ -2,5 +2,5 @@
 from .utils import init
 from .assistant_manager import *
 from .thread_manager import *
+from .tool_functions_map import *
 from .openai_assistant import OpenAIAssistant
-from .tool_functions_map import register_function, register_functions
--- openai_assistant/utils.py
+++ openai_assistant/utils.py
@@ -2,22 +2,52 @@
 import base64
 import io
 
-assistant_vision_support = None
-_openai = None
+_assistant_vision_support = None
+_openai_client = None
 
 
 def init(openai_client, assistant_vision_support=True):
-    global _openai
-    _openai = openai_client
+    """
+    Initialize the OpenAI client and assistant vision support.
+
+    Args:
+        openai_client: The OpenAI client instance.
+        assistant_vision_support (bool): Flag to support vision functionality. Defaults to True.
+    """
+    global _openai_client, _assistant_vision_support
+    _openai_client = openai_client
+    _assistant_vision_support = assistant_vision_support
 
 
 def get_openai_client():
-    if _openai is None:
+    """
+    Get the initialized OpenAI client.
+    Only for this package's internal use.
+
+    Returns:
+        The OpenAI client instance.
+
+    Raises:
+        ValueError: If the OpenAI client is not initialized.
+    """
+    if _openai_client is None:
         raise ValueError("OpenAI client is not initialized. Call `init(openai_client)` first.")
-    return _openai
+    return _openai_client
 
 
 def convert_base64_image(base64_image):
+    """
+    Convert a base64 encoded image to a BytesIO object.
+
+    Args:
+        base64_image (str): The base64 encoded image string.
+
+    Returns:
+        tuple: A tuple containing the BytesIO object and the image mimetype.
+
+    Raises:
+        ValueError: If the image type is unsupported.
+    """
     if base64_image.startswith('data:image/png;base64,'):
         base64_image_cleaned = base64_image[len('data:image/png;base64,'):]
         mimetype = 'image/png'
@@ -37,9 +67,9 @@ def convert_base64_image(base64_image):
     else:
         raise ValueError("Unsupported image type")
 
+    # Ensure base64 string is properly padded
     missing_padding = len(base64_image_cleaned) % 4
     if missing_padding:
-        print("Missing padding")
         base64_image_cleaned += '=' * (4 - missing_padding)
 
     image_data = base64.b64decode(base64_image_cleaned)
